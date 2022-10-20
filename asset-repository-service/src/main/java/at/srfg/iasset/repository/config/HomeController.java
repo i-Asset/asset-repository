59418311e31f30b6f5fb03380f25019312f5e0e4
fstroh <fstroh@gmail.com>
2022-10-20 14:16:39 +0200

    Add OpenAPIDefinition (servers)

--- asset-repository-service/src/main/java/at/srfg/iasset/repository/config/HomeController.java
+++ asset-repository-service/src/main/java/at/srfg/iasset/repository/config/HomeController.java
@@ -1,5 +1,8 @@
 package at.srfg.iasset.repository.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.annotations.servers.ServerVariable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Home redirection to swagger api documentation 
  */
 @Controller
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "{protocol}://{servername}:{port}/{basePath}",
+                        description = "Configurable Server URL",
+                        variables = {
+                                @ServerVariable(name = "protocol",
+                                        description = "http | https",
+                                        defaultValue = "http"),
+                                @ServerVariable(name = "servername", description = "IP address or server name",
+                                        defaultValue = "localhost"),
+                                @ServerVariable(name = "port", description = "Server port",
+                                        defaultValue = "8081"),
+                                @ServerVariable(name = "basePath", defaultValue = "")
+                        }),
+                @Server(url = "https://iasset.salzburgresearch.at/repository-service/",
+                        description = "Asset Repository Service on the i-Asset Staging Server")
+        }
+)
+@RequestMapping(value = "/")
 public class HomeController {
     @RequestMapping(value = "/")
     public String index() {
