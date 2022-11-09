e0a705fcb0e88a383e3758513d6c2335a4e4afbc
Martin Brugger <m.brugger@icosense.com>
2022-11-09 15:56:27 +0100

    add Fault Submodel

---
+++ asset-repository-api/src/main/java/at/srfg/iasset/repository/model/Fault.java
@@ -0,0 +1,11 @@
+package at.srfg.iasset.repository.model;
+
+import java.util.Date;
+
+public class Fault {
+	public String assetId;
+	public String subElementPath;
+	public String faultId;
+	public Date timestampCreated;
+
+}
