b3f416671e3febb32a892b2eb9fc03b31a6b0fa1
fstroh <fstroh@gmail.com>
2023-04-27 12:00:39 +0200

    update LocalServiceEnvironment tests

--- asset-connector/src/test/java/at/srfg/iasset/connector/environment/LocalServiceEnvironmentTest.java
+++ asset-connector/src/test/java/at/srfg/iasset/connector/environment/LocalServiceEnvironmentTest.java
@@ -1,18 +1,18 @@
 package at.srfg.iasset.connector.environment;
 
 import at.srfg.iasset.connector.component.impl.AASFull;
-import at.srfg.iasset.repository.model.helper.visitor.ReferenceCollector;
-import org.eclipse.aas4j.v3.model.KeyTypes;
-import org.eclipse.aas4j.v3.model.Reference;
 import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Set;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,10 +32,11 @@ class LocalServiceEnvironmentTest {
 
     @BeforeEach
     void init() throws URISyntaxException {
-        log.info("create Local Service Environment with repositoryURI '{}' ", repositoryUriString);
         URI repositoryURI = new URI(repositoryUriString);
         serviceEnvironmentUnderTest = new LocalServiceEnvironment(repositoryURI);
+        log.info("LocalServiceEnvironment with dummy repositoryURI '{}' created", repositoryUriString);
         loadAASTestData();
+        log.info("test data loaded");
     }
 
     @AfterEach
@@ -45,7 +46,7 @@ class LocalServiceEnvironmentTest {
     @DisplayName("Starting and Stopping of Local Service Environment")
     @Test
     void startAndStopEndpoint() {
-        final int port = startEndpoint();
+        final int port = startEndpointOnFreePort();
         assertNotEquals(0, port);
 
         log.info("local service environment started endpoint with port {}", port);
@@ -64,21 +65,77 @@ class LocalServiceEnvironmentTest {
         stopEventProcessor();
     }
 
-    @DisplayName("Register AAS with central repository")
-    @Disabled
+    @DisplayName("Basic local data loaded to connector")
     @Test
-    void register() {
-        startEndpoint();
-        assertTrue(serviceEnvironmentUnderTest.register("https://acplt.org/Test_AssetAdministrationShell"));
-        //
-        final String id = AASFull.AAS_BELT_INSTANCE.getId();
-        //serviceEnvironmentUnderTest.unregister(id);
-        //assertTrue(serviceEnvironmentUnderTest.register(id));
+    void basicLocalData() {
+        startEndpointOnFreePort();
 
+        final String id = AASFull.AAS_BELT_INSTANCE.getId();
         assertTrue(serviceEnvironmentUnderTest.getAssetAdministrationShell(id).isPresent());
         assertEquals(AASFull.AAS_BELT_INSTANCE, serviceEnvironmentUnderTest.getAssetAdministrationShell(id).get());
+
         stopEndpoint();
     }
+
+    @DisplayName("Register AAS with central repository")
+    @Test
+    void register() {
+        startEndpointOnFreePort();
+        final String id = AASFull.AAS_BELT_INSTANCE.getId();
+        // expecting false, because this requires a remote running asset repo which
+        // takes the registration (not available at "repositoryUriString")
+        assertFalse(serviceEnvironmentUnderTest.register(id));
+
+        // no response expected (void)
+        serviceEnvironmentUnderTest.unregister(id);
+
+        stopEndpoint();
+    }
+
+    @DisplayName("Load data from a central repository")
+    @Test
+    void initWithRemoteData() {
+        startEndpointOnFreePort();
+        // expecting exception, because this requires a remote running asset repo which
+        // hosts the requested data (not available at "repositoryUriString")
+        assertThrows(IllegalArgumentException.class, this::loadAASTestDataWithRemoteModels, "expected IllegalArgumentException!");
+
+        stopEndpoint();
+    }
+
+    @DisplayName("Test via REST")
+    @Test
+    void restTest() {
+        int port = startEndpointOnFreePort();
+        URI url = URI.create("http://localhost:"+port+"/shells");
+        RestTemplate rest = new RestTemplate();
+        final ResponseEntity<List> forEntity = rest.getForEntity(url, List.class);
+        assertNotNull(forEntity);
+        List body = forEntity.getBody();
+        assertNotNull(body);
+        assertEquals(6, body.size(), "expecting seven preloaded shells");
+        log.info("response: {}", body);
+        assertNotNull(body.get(0), "expecting element");
+        assertEquals(body.get(0).getClass(), LinkedHashMap.class, "expecting a linked hash map in body");
+    }
+
+    @DisplayName("Empty test")
+    @Test
+    void test() {
+        /*
+        startEndpointOnFreePort();
+        loadAASTestData();
+        assertEquals(6, serviceEnvironmentUnderTest.getAllAssetAdministrationShells().size());
+
+        Set<Reference> references = new ReferenceCollector(serviceEnvironmentUnderTest, KeyTypes.BASIC_EVENT_ELEMENT).collect(AASFull.AAS_BELT_INSTANCE);
+        assertEquals(1,references.size());
+
+        assertTrue(serviceEnvironmentUnderTest.getAssetAdministrationShell(AASFull.AAS_BELT_INSTANCE.getId()).isPresent());
+        log.info("config: {}", serviceEnvironmentUnderTest.getAllAssetAdministrationShells());
+
+         */
+    }
+
 
     private void stopEventProcessor() {
         serviceEnvironmentUnderTest.getEventProcessor().stopEventProcessing();
@@ -88,7 +145,7 @@ class LocalServiceEnvironmentTest {
         serviceEnvironmentUnderTest.shutdownEndpoint();
     }
 
-    private int startEndpoint() {
+    private int startEndpointOnFreePort() {
         try (ServerSocket serverSocket = new ServerSocket(0)) {
             assertNotNull(serverSocket);
             final int localPort = serverSocket.getLocalPort();
@@ -115,20 +172,25 @@ class LocalServiceEnvironmentTest {
         serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_1.getId(), AASFull.SUBMODEL_4.getId(), AASFull.SUBMODEL_4);
         serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_1.getId(), AASFull.SUBMODEL_5.getId(), AASFull.SUBMODEL_5);
         serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_1.getId(), AASFull.SUBMODEL_6.getId(), AASFull.SUBMODEL_6);
-        //serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_1.getId(), AASFull.SUBMODEL_7.getId(), AASFull.SUBMODEL_7);
         serviceEnvironmentUnderTest.setConceptDescription(AASFull.CONCEPT_DESCRIPTION_1.getId(), AASFull.CONCEPT_DESCRIPTION_1);
         serviceEnvironmentUnderTest.setConceptDescription(AASFull.CONCEPT_DESCRIPTION_2.getId(), AASFull.CONCEPT_DESCRIPTION_2);
         serviceEnvironmentUnderTest.setConceptDescription(AASFull.CONCEPT_DESCRIPTION_3.getId(), AASFull.CONCEPT_DESCRIPTION_3);
         serviceEnvironmentUnderTest.setConceptDescription(AASFull.CONCEPT_DESCRIPTION_4.getId(), AASFull.CONCEPT_DESCRIPTION_4);
-        //
         serviceEnvironmentUnderTest.setAssetAdministrationShell(AASFull.AAS_BELT_TEMPLATE.getId(), AASFull.AAS_BELT_TEMPLATE);
         serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_PROPERTIES_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_PROPERTIES_TEMPLATE);
-        //serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_EVENT_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_EVENT_TEMPLATE);
         serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_OPERATIONS_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_OPERATIONS_TEMPLATE);
+
         // belt instance data
         serviceEnvironmentUnderTest.setAssetAdministrationShell(AASFull.AAS_BELT_INSTANCE.getId(), AASFull.AAS_BELT_INSTANCE);
         serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_INSTANCE.getId(), AASFull.SUBMODEL_BELT_PROPERTIES_INSTANCE.getId(), AASFull.SUBMODEL_BELT_PROPERTIES_INSTANCE);
-        //serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_INSTANCE.getId(), AASFull.SUBMODEL_BELT_EVENT_INSTANCE.getId(), AASFull.SUBMODEL_BELT_EVENT_INSTANCE);
         serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_INSTANCE.getId(), AASFull.SUBMODEL_BELT_OPERATIONS_INSTANCE.getId(), AASFull.SUBMODEL_BELT_OPERATIONS_INSTANCE);
+    }
+    private void loadAASTestDataWithRemoteModels() {
+        serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_1.getId(), AASFull.SUBMODEL_7.getId(), AASFull.SUBMODEL_7);
+
+        serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_EVENT_TEMPLATE.getId(), AASFull.SUBMODEL_BELT_EVENT_TEMPLATE);
+
+        serviceEnvironmentUnderTest.setSubmodel(AASFull.AAS_BELT_INSTANCE.getId(), AASFull.SUBMODEL_BELT_EVENT_INSTANCE.getId(), AASFull.SUBMODEL_BELT_EVENT_INSTANCE);
+
     }
 }
