1d651506ff86be7f8a5808e6441d491bab48c027
fstroh <fstroh@gmail.com>
2023-06-22 13:06:46 +0200

    getters/setters toSttring for PlantElement

--- asset-repository-api/src/main/java/at/srfg/iasset/repository/model/PlantElement.java
+++ asset-repository-api/src/main/java/at/srfg/iasset/repository/model/PlantElement.java
@@ -9,4 +9,47 @@ public class PlantElement {
     List<String> identifiers;
     PlantElement parent;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getIdentifiers() {
+        return identifiers;
+    }
+
+    public void setIdentifiers(List<String> identifiers) {
+        this.identifiers = identifiers;
+    }
+
+    public PlantElement getParent() {
+        return parent;
+    }
+
+    public void setParent(PlantElement parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public String toString() {
+        return "PlantElement{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", identifiers=" + identifiers +
+                ", parent=" + parent +
+                '}';
+    }
+
+    
 }
