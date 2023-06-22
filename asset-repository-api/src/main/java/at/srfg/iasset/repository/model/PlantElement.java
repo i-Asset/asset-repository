b22837f62b570029b462c22f6b6c5ef00644f2e6
fstroh <fstroh@gmail.com>
2023-06-22 13:05:01 +0200

    element model for plant structure

---
+++ asset-repository-api/src/main/java/at/srfg/iasset/repository/model/PlantElement.java
@@ -0,0 +1,12 @@
+package at.srfg.iasset.repository.model;
+
+import java.util.List;
+
+public class PlantElement {
+
+    String name;
+    String description;
+    List<String> identifiers;
+    PlantElement parent;
+
+}
