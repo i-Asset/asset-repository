d41196172dd578c210a58a59ddc80356b2f7e865
Bernhard Adelsmair <b.adelsmair@ispro-ng.at>
2023-06-22 13:42:11 +0200

    problems with webapi call (generics)

--- asset-connector/src/main/java/at/srfg/iasset/connector/IsproNGConnector.java
+++ asset-connector/src/main/java/at/srfg/iasset/connector/IsproNGConnector.java
@@ -161,6 +161,7 @@ public class IsproNGConnector {
 
             //createFaultTest(connector);
 
+            var a = isproAPI.GetObjectPlantStructure();
             System.in.read();
             // shutdown
             connector.stop();
